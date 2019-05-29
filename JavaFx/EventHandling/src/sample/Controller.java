884810189d6e856b4bed5e0105878c6e0989c7f7
Sachin <barpandasachin@gmail.com>
2019-05-30 00:49:16 +0530

    Java FX Added

--- JavaFx/EventHandling/src/sample/Controller.java
+++ JavaFx/EventHandling/src/sample/Controller.java
@@ -1,14 +1,37 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
 
 public class Controller {
     @FXML
     private TextField textField;
+    @FXML
+    private Button HelloButton;
+    @FXML
+    private Button ByeButton;
 
     @FXML
-    public void onButtonClicked(){
-        System.out.println("Hello guys it's working!" + textField.getText());
+    public void initialize(){
+        HelloButton.setDisable(true);
+        ByeButton.setDisable(true);
+    }
+    @FXML
+    public void onButtonClicked(ActionEvent e){
+        if(e.getSource().equals(HelloButton)){
+            System.out.println("Hello "+ textField.getText());
+        }else if(e.getSource().equals(ByeButton)){
+            System.out.println("Bye "+textField.getText());
+        }
+    }
+
+    @FXML
+    public void handleKeyRelesed(){
+        String text = textField.getText();
+        boolean disableButtons = text.isEmpty()|| text.trim().isEmpty();
+        HelloButton.setDefaultButton(disableButtons);
+        ByeButton.setDefaultButton(disableButtons);
     }
 }
